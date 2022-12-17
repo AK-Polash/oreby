11b2edd3c205f8f7bf145f13956ab40bd2f94c4f
AK-Polash <polashk199@gmail.com>
2022-12-18 03:27:19 +0600

    slick slider done without responsive

--- src/components/layout/Banner.js
+++ src/components/layout/Banner.js
@@ -1,42 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "./Image";
+import { Link } from "react-router-dom";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 
 const Banner = () => {
+  let [dotActive, setDotActive] = useState("");
+
   const settings = {
+    dots: true,
+    beforeChange: (prev, next) => {
+      setDotActive(next);
+    },
     arrows: false,
-    dots: false,
+    autoplay: true,
+    autoplaySpeed: 3200,
     infinite: true,
+    cssEase: "linear",
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    appendDots: (dots) => (
+      <div
+        style={{
+          backgroundColor: "gray",
+        }}
+      >
+        <ul
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "5%",
+            transform: "translateY(-50%)",
+          }}
+        >
+          {dots}
+        </ul>
+      </div>
+    ),
+    customPaging: (i) => (
+      <div
+        style={
+          i == dotActive
+            ? {
+                width: "22px",
+                color: "#262626",
+                padding: "12.5px 0",
+                borderRight: "2px solid #262626",
+                fontFamily: "DM Sans",
+                fontSize: "10px",
+                cursor: "pointer",
+              }
+            : {
+                width: "22px",
+                color: "transparent",
+                padding: "12.5px 0",
+                borderRight: "2px solid #fff",
+                fontFamily: "DM Sans",
+                fontSize: "10px",
+                cursor: "pointer",
+              }
+        }
+      >
+        0{i + 1}
+      </div>
+    ),
   };
 
   return (
     <>
       <Slider {...settings}>
-        <div className="w-full">
-          <Image
-            className="w-full"
-            source="assets/banner_one.png"
-            alt="banner one"
-          />
-        </div>
-        <div className="w-full">
-          <Image
-            className="w-full"
-            source="assets/banner_one.png"
-            alt="banner two"
-          />
-        </div>
-        <div className="w-full">
-          <Image
-            className="w-full"
-            source="assets/banner_one.png"
-            alt="banner three"
-          />
-        </div>
+        <Link to="#">
+          <div className="w-full">
+            <Image
+              className="w-full"
+              source="assets/banner_one.png"
+              alt="banner one"
+            />
+          </div>
+        </Link>
+        <Link to="#">
+          <div className="w-full">
+            <Image
+              className="w-full"
+              source="assets/banner_one.png"
+              alt="banner two"
+            />
+          </div>
+        </Link>
+        <Link to="#">
+          <div className="w-full">
+            <Image
+              className="w-full"
+              source="assets/banner_one.png"
+              alt="banner three"
+            />
+          </div>
+        </Link>
       </Slider>
     </>
   );
